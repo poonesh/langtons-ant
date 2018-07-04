88d3026ecca5a66fee457030d25731cd004542db
pooneh <poonehshooshtari@gmail.com>
2018-07-04 17:29:05 -0400

    Exported the InfiniteGrid class

--- infiniteGrid.js
+++ infiniteGrid.js
@@ -2,21 +2,21 @@
 const BLACK = 0;
 const WHITE = 1;
 
-function initialCellColorChecked(row, col) {
-	if (row % 2 === 0 && col % 2 === 0){
+function initialCellColorChecked(x, y) {
+	if (x % 2 === 0 && y % 2 === 0){
 		return WHITE;
-	}else if (row % 2 !== 0 && col % 2 !== 0){
+	}else if (x % 2 !== 0 && y % 2 !== 0){
 		return WHITE;
 	}else{
 		return BLACK;
 	}
 }
 
-function initialCellColorWhite(row, col) {
+function initialCellColorWhite(x, y) {
 	return WHITE;
 }
 
-function initialCellColorBlack(row, col) {
+function initialCellColorBlack(x, y) {
 	return BLACK;
 }
 
@@ -31,39 +31,40 @@ class Node{
 	}
 }
 
-class InfiniteGrid{
-	constructor(nodes, initialCellColorFunction = initialCellColorChecked){
-		this.nodes = nodes;
+export class InfiniteGrid{
+	constructor(initialCellColorFunction = initialCellColorChecked){
 		this.visited = {};
+		// callback function to give the option to the user for which color 
+		// should be the background of the grid (white, black or checked)
 		this.initialCellColorFunction = initialCellColorFunction;
 	}
 
 	/*this function returns the current color of each cell*/
-	getCellColor(row, col){
-		if (self.visited[row] === undefined || self.visited[row][col] === undefined){
-			cellColor = this.initialCellColorFunction(row, col);
+	getCellColor(x, y){
+		if (self.visited[x] === undefined || self.visited[x][y] === undefined){
+			cellColor = this.initialCellColorFunction(x, y);
 			return cellColor
 		}
-		return self.visited[row][col].color;
+		return self.visited[x][col].color;
 	}
 
 	/*toggling the color of each cell. if the cell has been visited before just toggling the color 
 	otherwise the node should be added to the nested dictionary. the node will be created and the
 	initial color will be set there*/
-	toggleCellColor(row, col){
-		if (self.visited[row] !== undefined && self.visited[row][col] !== undefined){
+	toggleCellColor(x, y){
+		if (self.visited[x] !== undefined && self.visited[x][y] !== undefined){
 			node.color = node.color === WHITE ? BLACK : WHITE;
 			return;
 		}
 
-		if (self.visited[row] === undefined){
-			self.visited[row] = {};
+		if (self.visited[x] === undefined){
+			self.visited[x] = {};
 		}
 
-		let cellColor = this.initialCellColorFunction(row, col);
+		let cellColor = this.initialCellColorFunction(x, y);
 		cellColor = cellColor === WHITE ? BLACK : WHITE;
 		const node = Node(cellColor)
-		self.visited[row][col] = node;
+		self.visited[x][y] = node;
 
 	}
 }
